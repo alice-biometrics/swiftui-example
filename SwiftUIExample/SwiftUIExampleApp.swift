e5b93d2414961dcafc3491257a4e59b1c676862b
garciagarciaadrian <agarcia@alicebiometrics.com>
2023-06-13 14:37:59 +0200

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Adrian García on 13/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
